455d43fd7d10c381e980f634b42a229b903db0b9
Kamil <kamil@kamil.com>
2018-05-20 16:29:36 +0200

    Setup MongoDB Model

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express'); // Fast, unopinionated, minimalist web framework for node.
+const router = express.Router(); // Creates a new router object.
 const app = express(); // Initiate Express Application
 const mongoose = require('mongoose'); // Node Tool for MongoDB
 const config = require('./config/database'); // Mongoose Config
 const path = require('path'); // NodeJS Package for file paths
+const authentication = require('./routes/authentication')(router); // Import Authentication Routes
+const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+
 
 // Database Connection
 mongoose.Promise = global.Promise;
@@ -15,9 +19,12 @@ mongoose.connect(config.uri, (err) => {
 });
 
 // Provide static directory for frontend
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
 app.use(express.static(__dirname + '/client/dist/client/'));
+app.use('/authentication', authentication);
 
-// Connect server to Angular 2 Index.html
+// Connect server to Angular 4 Index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
 });
