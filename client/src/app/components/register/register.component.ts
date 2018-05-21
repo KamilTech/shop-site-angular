b3cd0c49c4456f2ea2f62b3d17eeb4f9ee7f40f5
Kamil <kamil@kamil.com>
2018-05-21 15:46:09 +0200

    Registration api and frontend Validation

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.createForm(); // Create Angular 2 Form when component loads
+  }
+
+  // Function to create registration form
+  createForm() {
+    this.form = this.formBuilder.group({
+      // Email Input
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+        this.validateEmail // Custom validation
+      ])],
+      // Username Input
+      username: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(15),
+        this.validateUsername // Custom validation
+      ])],
+      // Password Input
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(35),
+        this.validatePassword // Custom validation
+      ])],
+      // Confirm Password Input
+      confirm: ['', Validators.required] // Field is required
+    }, { validator: this.matchingPasswords('password', 'confirm') }); // Add custom validator to form for matching passwords
+  }
+
+  // Function to validate e-mail is proper format
+  validateEmail(controls) {
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (regExp.test(controls.value)) {
+      return null;
+    } else {
+      return { 'validateEmail': true } 
+    }
+  }
+
+  // Function to validate username is proper format
+  validateUsername(controls) {
+    const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+    if (regExp.test(controls.value)) {
+      return null; 
+    } else {
+      return { 'validateUsername': true }
+    }
+  }
+
+  // Function to validate password
+  validatePassword(controls) {
+    const regExp = new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/);
+    if (regExp.test(controls.value)) {
+      return null; 
+    } else {
+      return { 'validatePassword': true } 
+    }
+  }
+
+  // Funciton to ensure passwords match
+  matchingPasswords(password, confirm) {
+    return (group: FormGroup) => {
+      if (group.controls[password].value === group.controls[confirm].value) {
+        return null;
+      } else {
+        return { 'matchingPasswords': true } 
+      }
+    }
+  }
+
+  // Function to submit form
+  onRegisterSubmit() {
+    console.log('form submitted');
+  }
 
   ngOnInit() {
   }
